f8195a6348da2614f2172bff007a76ef913dcf59
Jerome Leclanche <jerome@leclan.ch>
2016-07-10 12:51:05 +0300

    Rewrite scen_to_dbf.py to use the hearthstone.dbf module

--- scen_to_dbf.py
+++ scen_to_dbf.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import os.path
 import sys
-from xml.dom import minidom
-from xml.etree import ElementTree
+from collections import OrderedDict
+from lxml.etree import tostring
+from hearthstone.dbf import Dbf
 from hearthstone.enums import Locale
 
 
@@ -16,92 +17,102 @@ except ImportError as e:
 	sys.exit(1)
 
 
-column_map = {
-	"num_players": "PLAYERS",
-	"adventure_mode_id": "MODE_ID",
-}
+class ScenarioProtoDBF(Dbf):
+	name = "SCENARIO"
+	source_fingerprint = None
+	columns = OrderedDict([
+		("ID", "Int"),
+		("NOTE_DESC", "String"),
+		("PLAYERS", "Int"),
+		("PLAYER1_HERO_CARD_ID", "Int"),
+		("PLAYER2_HERO_CARD_ID", "Int"),
+		("IS_TUTORIAL", "Bool"),
+		("IS_EXPERT", "Bool"),
+		("IS_COOP", "Bool"),
+		("ADVENTURE_ID", "Int"),
+		("WING_ID", "Int"),
+		("SORT_ORDER", "Int"),
+		("MODE_ID", "Int"),
+		("CLIENT_PLAYER2_HERO_CARD_ID", "Int"),
+		("NAME", "LocString"),
+		("SHORT_NAME", "LocString"),
+		("DESCRIPTION", "LocString"),
+		("OPPONENT_NAME", "LocString"),
+		("COMPLETED_DESCRIPTION", "LocString"),
+		("PLAYER1_DECK_ID", "Int"),
+		("DECK_BUILDER_ID", "Int"),
+		("DECK_RULESET_ID", "Int"),
+		("TB_TEXTURE", "AssetPath"),
+		("TB_TEXTURE_PHONE", "AssetPath"),
+		("TB_TEXTURE_PHONE_OFFSET_Y", "Float"),
+	])
 
-column_names = [
-	"ID", "NOTE_DESC", "PLAYERS", "PLAYER1_HERO_CARD_ID", "PLAYER2_HERO_CARD_ID",
-	"IS_TUTORIAL", "IS_EXPERT", "IS_COOP", "ADVENTURE_ID", "MODE_ID", "NAME",
-	"SHORT_NAME", "DESCRIPTION", "COMPLETED_DESCRIPTION", "WING_ID", "SORT_ORDER",
-	"CLIENT_PLAYER2_HERO_CARD_ID", "OPPONENT_NAME", "TAVERN_BRAWL_TEXTURE",
-	"TAVERN_BRAWL_TEXTURE_PHONE", "TAVERN_BRAWL_TEXTURE_PHONE_OFFSET_X",
-	"TAVERN_BRAWL_TEXTURE_PHONE_OFFSET_Y",
-]
+	COLUMN_MAP = {
+		"num_players": "PLAYERS",
+		"adventure_mode_id": "MODE_ID",
+		"tavern_brawl_texture": "TB_TEXTURE",
+		"tavern_brawl_texture_phone": "TB_TEXTURE_PHONE",
+		"tavern_brawl_texture_phone_offset": "TB_TEXTURE_PHONE_OFFSET_Y",
+	}
 
+	def __init__(self):
+		self.records = []
 
-def get_column_name(field_name):
-	return column_map.get(field_name, field_name.upper())
+	@classmethod
+	def deserialize_locstrings(cls, strings):
+		ret = {}
+		for string in strings:
+			ret[string.key] = {}
+			for value in string.values:
+				try:
+					locale = Locale(value.locale).name
+					ret[string.key][locale] = value.value
+				except Exception:
+					sys.stderr.write("WARNING: Invalid locale %r\n" % (value.locale))
 
+		return ret
 
-def pretty_xml(xml):
-	ret = ElementTree.tostring(xml)
-	ret = minidom.parseString(ret).toprettyxml(indent="\t")
-	return "\n".join(line for line in ret.split("\n") if line.strip())
+	def get_column_name(self, name):
+		return self.COLUMN_MAP.get(name, name.upper())
 
+	def load_proto(self, f):
+		proto = ScenarioDbRecord()
+		proto.ParseFromString(f.read())
 
-def proto_to_xml(record):
-	xml_record = ElementTree.Element("Record")
-	values = decompile_proto(record)
-	for field_name in column_names:
-		if field_name == "IS_TUTORIAL":
-			value = False
-		elif field_name in ("COMPLETED_DESCRIPTION", "OPPONENT_NAME"):
-			value = ""
-		else:
-			value = values[field_name]
-		e = ElementTree.Element("Field")
-		e.attrib["column"] = field_name
-		if isinstance(value, dict):
-			# Turn LocStrings into a sub-tree of locale elements
-			for locale in Locale:
-				if locale.name not in value:
-					continue
-				locale_elem = ElementTree.Element(locale.name)
-				locale_elem.text = value[locale.name]
-				e.append(locale_elem)
-		else:
-			e.text = str(value)
-		xml_record.append(e)
+		record = {
+			"IS_TUTORIAL": False,
+			"OPPONENT_NAME": None,
+			"COMPLETED_DESCRIPTION": None,
+			"PLAYER1_DECK_ID": None,
+			"DECK_BUILDER_ID": None,
+			"DECK_RULESET_ID": None,
+		}
 
-	return xml_record
+		for field in ScenarioDbRecord.DESCRIPTOR.fields:
+			value = getattr(proto, field.name)
+			if field.name == "strings":
+				record.update(self.deserialize_locstrings(value))
+				continue
+			column = self.get_column_name(field.name)
 
+			if column == "TB_TEXTURE_PHONE_OFFSET_Y":
+				value = value.y
 
-def decompile_proto(record):
-	values = {}
-	for string in record.strings:
-		strings = {}
-		for value in string.values:
-			try:
-				locale = Locale(value.locale).name
-				strings[locale] = value.value
-			except Exception:
-				sys.stderr.write("WARNING: Invalid locale %r\n" % (value.locale))
-		values[string.key] = strings
+			record[column] = value
 
-	for field in ScenarioDbRecord.DESCRIPTOR.fields:
-		field_name = get_column_name(field.name)
-		value = getattr(record, field.name)
-		if field.name == "tavern_brawl_texture_phone_offset":
-			values[field_name + "_X"] = value.x
-			values[field_name + "_Y"] = value.y
-		else:
-			values[field_name] = str(value)
-	return values
+		self.records.append(record)
 
 
 def main():
-	root = ElementTree.Element("Dbf")
+	dbf = ScenarioProtoDBF()
 
 	for filename in sys.argv[1:]:
 		with open(filename, "rb") as f:
-			record = ScenarioDbRecord()
-			record.ParseFromString(f.read())
-			xml_record = proto_to_xml(record)
-			root.append(xml_record)
+			dbf.load_proto(f)
 
-	print(pretty_xml(root))
+	xml = dbf._to_xml()
+	ret = tostring(xml, encoding="utf-8", pretty_print=True, xml_declaration=True)
+	print(ret.decode("utf-8"))
 
 
 if __name__ == "__main__":
